c6e1d0ba51160d362356f9c25db91224ff7af29e
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-07-20 22:42:24 -0300

    add-git-project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from "miragejs"
+import { App } from './App';
+import { transitions } from 'polished';
+import { removeListener } from 'node:process';
+
+ createServer({
+   models: {
+     transaction: Model,
+   },
+
+   seeds(server) {
+     server.db.loadData({
+       transactions: [
+         {
+           id: 1,
+           title: "Freelance de website",
+           type: "deposit",
+           category: "Dev",
+           amount: 6000,
+           createdAt: new Date("2022-05-13 21:44:00"),
+         },
+         {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2022-05-20 12:44:00"),
+        }
+       ]
+     })
+   },
+
+  routes() {
+    this.namespace = "api"
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction")
+    })
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create("transaction", data)
+    })
+
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,9 +56,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+); 
